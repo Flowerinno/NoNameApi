d84e6d5f90438440c552a5a3b1838bf4d5a1731c
Aleksandr Kononov <oleksandr.kononov@devit.group>
2024-01-04 17:26:55 +0200

    add initial readme

--- app/utils/auth/getSession.ts
+++ app/utils/auth/getSession.ts
@@ -1,5 +1,10 @@
 import Cookies from "js-cookie";
 
+/**
+ * @description Get session
+ * @returns {boolean}
+ */
+
 export const getSession = () => {
 	return Cookies.get("__session") ? true : false;
 };
